4ae698c6aa38478d7467cf8780e5811d19a44984
Sidratul Islam <sidratul@topofstacksoftware.com>
2023-10-01 23:34:15 +0600

    refactor

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -31,204 +31,41 @@ const Project = async () => {
                 </a>
               </div>
               <div className="flex flex-wrap -mx-4 mb-4">
-                <div className="mb-8 w-full md:w-1/2 lg:w-1/3 px-4">
-                  <div className="bg-white rounded">
-                    <img
-                        className="rounded-t object-cover h-128"
-                        src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
-                        alt=""
-                    />
-                    <div className="p-6">
-                      <span className="text-gray-400">2021</span>
-                      <h3 className="mb-4 text-2xl font-bold font-heading">
-                        Lorem ipsum dolor sit amet consectutar
-                      </h3>
-                      <a
-                          className="flex text-green-600 hover:text-green-700 font-bold"
-                          href="#"
-                      >
-                        <svg
-                            className="mr-3 w-6 h-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                          <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                              clipRule="evenodd"
-                          />
-                        </svg>
-                        <span>View Case Study</span>
-                      </a>
+                {data.map((item, index) => (
+                    <div key={index} className="mb-8 w-full md:w-1/2 lg:w-1/3 px-4">
+                      <div className="bg-white rounded">
+                        <img
+                            className="rounded-t object-cover h-128"
+                            src={item.image}
+                            alt=""
+                        />
+                        <div className="p-6">
+                          <span className="text-gray-400">2021</span>
+                          <h3 className="mb-4 text-2xl font-bold font-heading">
+                            {item.title}
+                          </h3>
+                          <a
+                              className="flex text-green-600 hover:text-green-700 font-bold"
+                              href={item.live}
+                          >
+                            <svg
+                                className="mr-3 w-6 h-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                              <path
+                                  fillRule="evenodd"
+                                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                                  clipRule="evenodd"
+                              />
+                            </svg>
+                            <span>{item.remark}</span>
+                          </a>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div className="mb-8 w-full md:w-1/2 lg:w-1/3 px-4">
-                  <div className="bg-white rounded">
-                    <img
-                        className="rounded-t object-cover h-128"
-                        src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
-                        alt=""
-                    />
-                    <div className="p-6">
-                      <span className="text-gray-400">2021</span>
-                      <h3 className="mb-4 text-2xl font-bold font-heading">
-                        Curabitur imperdiet feugiat cursus
-                      </h3>
-                      <a
-                          className="flex text-green-600 hover:text-green-700 font-bold"
-                          href="#"
-                      >
-                        <svg
-                            className="mr-3 w-6 h-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                          <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                              clipRule="evenodd"
-                          />
-                        </svg>
-                        <span>View Case Study</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="mb-8 w-full md:w-1/2 lg:w-1/3 px-4">
-                  <div className="bg-white rounded">
-                    <img
-                        className="rounded-t object-cover h-128"
-                        src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=968&q=80"
-                        alt=""
-                    />
-                    <div className="p-6">
-                      <span className="text-gray-400">2021</span>
-                      <h3 className="mb-4 text-2xl font-bold font-heading">
-                        Nullam imperdiet lorem at accumsan interdum
-                      </h3>
-                      <a
-                          className="flex text-green-600 hover:text-green-700 font-bold"
-                          href="#"
-                      >
-                        <svg
-                            className="mr-3 w-6 h-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                          <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                              clipRule="evenodd"
-                          />
-                        </svg>
-                        <span>View Case Study</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="mb-8 w-full md:w-1/2 lg:w-1/3 px-4">
-                  <div className="bg-white rounded">
-                    <img
-                        className="rounded-t object-cover h-128"
-                        src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
-                        alt=""
-                    />
-                    <div className="p-6">
-                      <span className="text-gray-400">2021</span>
-                      <h3 className="mb-4 text-2xl font-bold font-heading">
-                        Lorem ipsum dolor sit amet consectutar
-                      </h3>
-                      <a
-                          className="flex text-green-600 hover:text-green-700 font-bold"
-                          href="#"
-                      >
-                        <svg
-                            className="mr-3 w-6 h-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                          <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                              clipRule="evenodd"
-                          />
-                        </svg>
-                        <span>View Case Study</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="mb-8 w-full md:w-1/2 lg:w-1/3 px-4">
-                  <div className="bg-white rounded">
-                    <img
-                        className="rounded-t object-cover h-128"
-                        src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
-                        alt=""
-                    />
-                    <div className="p-6">
-                      <span className="text-gray-400">2021</span>
-                      <h3 className="mb-4 text-2xl font-bold font-heading">
-                        Curabitur imperdiet feugiat cursus
-                      </h3>
-                      <a
-                          className="flex text-green-600 hover:text-green-700 font-bold"
-                          href="#"
-                      >
-                        <svg
-                            className="mr-3 w-6 h-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                          <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                              clipRule="evenodd"
-                          />
-                        </svg>
-                        <span>View Case Study</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="mb-8 w-full md:w-1/2 lg:w-1/3 px-4">
-                  <div className="bg-white rounded">
-                    <img
-                        className="rounded-t object-cover h-128"
-                        src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=968&q=80"
-                        alt=""
-                    />
-                    <div className="p-6">
-                      <span className="text-gray-400">2021</span>
-                      <h3 className="mb-4 text-2xl font-bold font-heading">
-                        Nullam imperdiet lorem at accumsan interdum
-                      </h3>
-                      <a
-                          className="flex text-green-600 hover:text-green-700 font-bold"
-                          href="#"
-                      >
-                        <svg
-                            className="mr-3 w-6 h-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                          <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                              clipRule="evenodd"
-                          />
-                        </svg>
-                        <span>View Case Study</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
+                ))}
               </div>
               <div className="text-center">
                 <a
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -22,222 +22,34 @@ const Testimonial = async () => {
               </p>
             </div>
             <div className="flex flex-wrap">
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
+              {data.map((item, index) => (
+                  <div key={index} className="w-full md:w-1/2 py-5 md:px-5">
+                    <div
+                        className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
+                        data-wow-delay=".1s"
+                    >
+                      <div className="flex items-center mb-4">
+                        <img
+                            className="h-16 w-16 rounded-full object-cover"
+                            src={item.image}
+                            alt="Monst"
+                        />
+                        <div className="pl-4">
+                          <strong className="mt-6 mb-2 text-md">
+                            {item.name}
+                          </strong>
+                          <p className="text-gray-500 text-xs mt-3">
+                            {item.designation}
+                          </p>
+                        </div>
+                      </div>
+                      <p className="leading-loose text-blueGray-400 mb-5">
+                        {item.msg}
                       </p>
                     </div>
                   </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
-                      </p>
-                    </div>
-                  </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
-                      </p>
-                    </div>
-                  </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
-                      </p>
-                    </div>
-                  </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
-                      </p>
-                    </div>
-                  </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
-                      </p>
-                    </div>
-                  </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
-                      </p>
-                    </div>
-                  </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
-              <div className="w-full md:w-1/2 py-5 md:px-5">
-                <div
-                    className="px-6 py-10 bg-white shadow rounded hover-up-5 wow animate__animated animate__fadeIn animated border border-gray-100 hover:border-gray-200"
-                    data-wow-delay=".1s"
-                >
-                  <div className="flex items-center mb-4">
-                    <img
-                        className="h-16 w-16 rounded-full object-cover"
-                        src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=900&t=st=1695802380~exp=1695802980~hmac=49dde71bdda7824e822a381cc2d3de915548e6eefbb10203a557f8202a828fdf"
-                        alt="Monst"
-                    />
-                    <div className="pl-4">
-                      <strong className="mt-6 mb-2 text-md">
-                        Geraldine Tusoy
-                      </strong>
-                      <p className="text-gray-500 text-xs mt-3">
-                        CEO, Co Founders
-                      </p>
-                    </div>
-                  </div>
-                  <p className="leading-loose text-blueGray-400 mb-5">
-                    Donec consequat tortor risus, at auctor felis consequat a.
-                    Donec quis dolor sem. Sed sollicitudin magna in hendrerit
-                    pulvinar. Vestibulum non quam velit.
-                  </p>
-                </div>
-              </div>
+              ))}
+
             </div>
           </div>
         </section>
